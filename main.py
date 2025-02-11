3c73a892d09f876ddf16480f90af20f93c095651
Percy <percy@railway.app>
2025-02-11 18:00:54 +0000

    Initial commit

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import os
 
 app = Flask(__name__)
 
+<<<<<<< HEAD
 @app.route('/hola', methods=['GET'])
 def hola():
     return jsonify({"mensaje": "Hola"}), 200 
+=======
+>>>>>>> 3a35824 (Initial commit)
 
 @app.route('/')
 def index():
