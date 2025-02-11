4f33809d7afdb6b4be0f7f0b5c5379665d358dc4
Victor Manuel Ninahuanca Ayala <victor.ninahuanca@agetic.gob.bo>
2025-02-11 14:05:02 -0400

    feat: actualizar mensaje

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import os
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 @app.route('/hola', methods=['GET'])
 def hola():
     return jsonify({"mensaje": "Hola"}), 200 
-=======
->>>>>>> 3a35824 (Initial commit)
 
 @app.route('/')
 def index():
